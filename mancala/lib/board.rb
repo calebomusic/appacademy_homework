35427aa84b001ed4ae92a13b2b4eb81b8a8e7037
calebomusic <calebomusic@gmail.com>
2016-09-19 20:57:04 -0700

    pass board specs

--- mancala/lib/board.rb
+++ mancala/lib/board.rb
@@ -1,21 +1,51 @@
+require 'byebug'
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    @cups[0..5] = @cups[0..5].map {|c| c += [:stone, :stone, :stone, :stone] }
+    @cups[7..12] = @cups[7..12].map {|c| c += [:stone, :stone, :stone, :stone] }
   end
 
   def valid_move?(start_pos)
+    raise 'Invalid starting cup' if !start_pos.between?(1,13)
   end
 
   def make_move(start_pos, current_player_name)
+    i = 0
+    until @cups[start_pos].empty?
+      pos = (start_pos + i) % 14
+      @cups[pos]<< @cups[start_pos].pop unless pos == opponents_cup(current_player_name)
+      i += 1
+    end
+    render
+    next_turn(pos, current_player_name)
   end
 
-  def next_turn(ending_cup_idx)
-    # helper method to determine what #make_move returns
+  def opponents_cup(name)
+    name == @name1 ? 13 : 6
+  end
+
+  def next_turn(ending_cup_idx, name)
+    # debugger
+    if ending_cup_idx == current_player_points_cup(name)
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
+  end
+
+  def current_player_points_cup(name)
+    name == @name1 ? 6 : 13
   end
 
   def render
@@ -27,8 +57,19 @@ class Board
   end
 
   def cups_empty?
+    # debugger
+    row_empty?(@cups[0..5]) || row_empty?(@cups[7..12])
+  end
+
+  def row_empty?(row)
+    row.each do |cup|
+      return false if cup.length >= 1
+    end
+    true
   end
 
   def winner
+    comparison = @cups[6] <=> @cups[13]
+    comparison == 1 ? @name1 : comparison == -1 ? @name2 : :draw
   end
 end
