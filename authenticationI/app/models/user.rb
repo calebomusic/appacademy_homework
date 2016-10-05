f62f63ae223c891b923819ac3c2580ce5365576d
calebomusic <calebomusic@gmail.com>
2016-10-04 20:55:06 -0700

    Add session_token to user

--- authenticationI/app/models/user.rb
+++ authenticationI/app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   attr_reader :password
-  
+
   validates :username, presence: true
   validates :password_digest, presence: {message: "Password can't be blank"}
   validates :password, length: { minimum: 6, allow_nil: true }
+  validates :session_token, presence: true
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
 
   def password=(password)
     @password = password
@@ -12,5 +18,16 @@ class User < ApplicationRecord
 
   def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 end
