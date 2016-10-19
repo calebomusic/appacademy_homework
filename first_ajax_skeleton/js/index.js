2ea0199d4562bd5233da98971b06667835187e4f
calebomusic <calebomusic@gmail.com>
2016-10-18 23:03:09 -0700

    Finish the first AJAX request in my entire life

--- first_ajax_skeleton/js/index.js
+++ first_ajax_skeleton/js/index.js
@@ -1,5 +1,15 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
-
+$.ajax({
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  type: "GET",
+  success(data) {
+    console.log('The weather is:');
+    console.log(data.weather[0].description);
+  },
+  error() {
+    console.error("An error occurred.");
+  }
+})
 // Add another console log here, outside your AJAX request
