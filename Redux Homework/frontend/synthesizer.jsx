bc24ece577ebb88fc0452844a02247483f0e02f3
calebomusic <calebomusic@gmail.com>
2016-10-24 21:31:34 -0700

    Finish Redux Homework: Intro to Middleware

--- Redux Homework/frontend/synthesizer.jsx
+++ Redux Homework/frontend/synthesizer.jsx
@@ -8,6 +8,24 @@ import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  store.dispatch = addLoggingToDispatch(store);
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
 });
+
+const addLoggingToDispatch = (store) => (next) => (action) => {
+    const Dispatch = store.dispatch;
+    console.log(action);
+    console.log(action);
+    let returnValue = Dispatch(action)
+    console.log(store.getState());
+    return returnValue;
+}
+
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  })
+  return Object.assign({}, store, { dispatch});
+}
